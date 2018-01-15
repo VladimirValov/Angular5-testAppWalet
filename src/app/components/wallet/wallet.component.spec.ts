abf50ed356c29eba9d1a68928f5d60ac401f27ce
Vladimir Valov <valov-web@yandex.ru>
2018-01-15 11:45:12 +0200

    add unit tests

--- src/app/components/wallet/wallet.component.spec.ts
+++ src/app/components/wallet/wallet.component.spec.ts
@@ -27,24 +27,28 @@ describe('WalletComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render current balance in the div.balance element', async(() => {
+  it('should render current balance in the div.balance element', () => {
     const div = compiled.querySelector('div.balance')
     expect(div).toBeTruthy();
     expect(div.textContent).toContain('429.29 ₽');
-  }));
+  });
 
-  it('should render input element', async(() => {
+  it('should render input element', () => {
     expect(compiled.querySelector('input')).toBeTruthy();
-  }));
+  });
 
-  it('should render new balance sync', async(() => {
+  it('should render button element', () => {
+    expect(compiled.querySelector('button')).toBeTruthy();
+  });
+
+  it('should render new balance sync', () => {
     const div = compiled.querySelector('div.balance');
     expect(div.textContent).toContain('429.29 ₽');
     
     component.balance = 1000;
     fixture.detectChanges();
     expect(div.textContent).toContain('1000.00 ₽');
-  }));
+  });
 
   it('should render new balance async', async(() => {
     const div = compiled.querySelector('div.balance');
@@ -61,4 +65,19 @@ describe('WalletComponent', () => {
     })
   }));
 
+  it('newBalance field validity', () => {
+    let errors = {};
+
+    let newBalance = component.balanceForm.controls['newBalance'];
+    errors = newBalance.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    newBalance.setValue(-1);
+    errors = newBalance.errors || {};
+    expect(errors['min']).toBeTruthy();
+
+    newBalance.setValue(10000000000);
+    errors = newBalance.errors || {};
+    expect(errors['max']).toBeTruthy();
+  });
 });
